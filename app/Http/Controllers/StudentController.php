7839177a26c1c61957602ab40420fe080ce3a2ca
Alexander040904 <jonathanalexanderramirezortega@gmail.com>
2024-12-04 18:40:35 -0600

    🐳 chore(migration, model, controller, route): Configuración inicial  Students
    Migración creada con los campos necesarios para la tabla.
    Rutas API definidas con el método.
    Controlador generado con métodos del CRUD.
    Modelo creado con asignación masiva configurada.

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    //
+    public function store(){
+
+    }
+    public function show(){
+
+    }
+    public function update(){
+
+    }
+    public function destroy(){
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('students', [StudentController::class,'store']);
+Route::get('students/{id}', [StudentController::class,'show']);
+Route::put('students/{id}', [StudentController::class,'update']);
+Route::delete('students/{id}', [StudentController::class,'destroy']);
